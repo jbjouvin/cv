34f73b8d17cd2da23ccfe4af240dfe4805b3d51f
JB <jb.jouvin@gmail.com>
2017-10-01 11:05:21 +0200

    Improvement...

--- js/cv.js
+++ js/cv.js
@@ -1,6 +1,19 @@
-function addMission(mission) {
-    var title = mission.title;
-    var tasks = mission.tasks
+function addTechnicalEnv(technicalEnv, title){
+    var techItem = technicalEnv;
+    workDiv = document.getElementById(title)
+    techItem.forEach(function (item) {
+        // console.log(task);
+    })
+}
+
+
+function addMission(mission, title) {
+    var missionTitle = mission.title;
+    var tasks = mission.tasks;
+    console.log(title)
+    // Ajout titre mission
+    workDiv = document.getElementById(title)
+    console.log(workDiv);
     tasks.forEach(function (task) {
         // console.log(task);
     })
@@ -12,27 +25,65 @@ function addExp(work) {
     var titleLong = work.titleLong;
     var company = work.company;
     var companyLink = work.companyLink;
+    var companyLogo = work.companyLogo;
     var place = work.place;
     var dateStart = work.dateStart;
     var dateEnd = work.dateEnd;
-    var technicalEnv = work.technicalEnv;
-    var mission = work.missions.forEach(function (mission) {
-        addMission(mission);
-    })
+
 
     // Create div
     var workDivElt = document.createElement("div");
-    workDivElt.className = "col s12"
+    workDivElt.className = "col s12";
+    workDivElt.id = title;
 
-    var titleElt = document.createElement("h5");
-    titleElt.textContent = title;
+    // Title + company + link + img
+    var titleLongElt = document.createElement("h5", "strong");
+    titleLongElt.textContent = titleLong + " chez ";
 
-    workDivElt.appendChild(titleElt)
+    var companyLinkElt = document.createElement("a")
+    companyLinkElt.textContent = company;
+    companyLinkElt.href = companyLink;
+    titleLongElt.appendChild(companyLinkElt);
 
+    var companyLogoElt = document.createElement("img");
+    companyLogoElt.src = companyLogo;
+    companyLogoElt.height = 90;
+    companyLogoElt.width = 90;
+    companyLogoElt.align = "right";
+    titleLongElt.appendChild(companyLogoElt);
+    workDivElt.appendChild(titleLongElt);
+
+    // lieu
+    var placeElt = document.createElement("span");
+    placeElt.textContent = place + ": ";
+    placeElt.className = "lieu";
+    workDivElt.appendChild(placeElt);
+
+    // date
+    var dateElt = document.createElement("span");
+    dateElt.textContent = dateStart + " à " + dateEnd;
+    dateElt.className = "dates";
+    workDivElt.appendChild(dateElt);
+
+    // Ajout Div
     document.getElementById("experiences").appendChild(workDivElt);
+
+
+    // ajout missions
+    var mission = work.missions.forEach(function (mission, title) {
+        addMission(mission, work.title);
+    })
+
+    //Ajout technicalEnv
+    var technicalEnv = work.technicalEnv.forEach(function (technicalEnv, title) {
+        addTechnicalEnv(technicalEnv, work.title);
+    })
+
 
 }
 
+
+// Lecture du fichier xp.json
 $.getJSON("xp.json", function (data) {
     // console.log(data);
     // console.log(Object.keys(data.work).length);
