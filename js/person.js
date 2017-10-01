e530b88016639b0e06fa129b8667296aeafb1726
JB <jb.jouvin@gmail.com>
2017-10-01 18:35:51 +0200

    working..

--- js/person.js
+++ js/person.js
@@ -1,6 +1,42 @@
 function initPerson(person){
     var name = person.name;
-    console.log(name)
+    var lastName = person.lastname;
+    var linkedin = person.linkedin;
+    var avatar = person.avatar;
+    var mail = person.mail;
+    var city = person.city;
+    var jobTitle = person.jobTitle;
+
+    //
+    var personDiv = document.getElementById("adminPhoto");
+    var avatarElt = document.createElement("img");
+    avatarElt.className = "rounded-circle";
+    avatarElt.align = "right";
+    avatarElt.border = "75px";
+    avatarElt.width = 175;
+    avatarElt.height = 175;
+    avatarElt.alt = name + " " +lastName;
+    avatarElt.src = avatar;
+
+    personDiv.appendChild(avatarElt);
+
+    // icone identity
+    var iconeElt = document.createElement("i");
+    iconeElt.className = "material-icons";
+    iconeElt.textContent = "perm_identity";
+    personDiv.appendChild(iconeElt);
+    
+    //city
+    var cityElt = document.createElement("a");
+    cityElt.textContent = city;
+    cityElt.href = "https://www.google.fr/maps/place/"+city;
+    
+    personDiv.appendChild(cityElt);
+
+    // <i class="material-icons">perm_identity</i>
+    // <a href="https://www.google.fr/maps/place/Paris">Paris, France</a>
+    // <a href="mailto:jb.jouvin@gmail.com">jb.jouvin@gmail.com</a>
+    // <a href="https://fr.linkedin.com/in/jean-baptiste-jouvin-35773258">Linkedin</a>
 }
 
 function initSkills(skills){
